f9d2ef7ccd06f5317334f9bd877aa668f09f56f2
Md. Nazmul Hossain <nazmulonline2002@gmail.com>
2025-05-15 00:37:35 +0600

    v1.7.2-CRC update

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ The BitGuard is a various error detection and correction techniques. Take input 
 [![X][x-shield]][x-url]
 
 [//]: # (Version)
-<!-- Another Version is start V1.7.1 -->
+<!-- Another Version is start V1.7.2 -->
 <div>
-  <img src="https://img.shields.io/badge/Version-1.7.1-blue?logo=Git&logoColor=green" height="25" alt="GiHub Version"> <br>
+  <img src="https://img.shields.io/badge/Version-1.7.2-cyan?logo=Git&logoColor=green" height="25" alt="GiHub Version"> <br>
 
 ###
 </div>
@@ -25,7 +25,7 @@ The BitGuard is a various error detection and correction techniques. The applica
 ## Features
 1. ![Static Badge](https://img.shields.io/badge/Home-90%25-cb410b)
 2. ![Static Badge](https://img.shields.io/badge/Hamming_Code-90%25-4a5d23)
-3. ![Static Badge](https://img.shields.io/badge/CRC-50%25-blue)
+3. ![Static Badge](https://img.shields.io/badge/CRC-90%25-blue)
 4. ![Static Badge](https://img.shields.io/badge/ChecksUm-75%25-800080)
 5. ![Static Badge](https://img.shields.io/badge/Stuff_DeStuff-90%25-008000)
 6. ![Static Badge](https://img.shields.io/badge/Manual-90%25-126180 )
--- src/main/java/com/bitguard/controllers/crc/crcController.java
+++ src/main/java/com/bitguard/controllers/crc/crcController.java
@@ -1,5 +1,6 @@
 package com.bitguard.controllers.crc;
 
+import com.bitguard.helper.BinaryCheck;
 import com.bitguard.models.crcModel.CRCModel;
 import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXRadioButton;
@@ -8,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,13 +18,13 @@ public class crcController implements Initializable {
     public TextField crcDataTF;
     public TextField crcKeyTF;
     public JFXCheckBox senderErrorCB;
-    public TextField senderErrorIndexTF;
     public JFXRadioButton encodingRB;
     public JFXRadioButton decodingRB;
     public TextField receiverDataTF;
     public Label errorMessage;
+    public Pane errorPicPane;
 
-    private String data, errorIndex, key, codingType;
+    private String data, key, codingType;
     private String encodedData, decodedData;
     private boolean validEncode;
 
@@ -40,40 +42,70 @@ public class crcController implements Initializable {
     private void collectData(){
         data = crcDataTF.getText();
         key = crcKeyTF.getText();
-        errorIndex = senderErrorIndexTF.getText();
         codingType = ((JFXRadioButton) codingTypeToggleGroup.getSelectedToggle()).getText();
-
-        printData();
     }
 
     private void printData(){
         System.out.println("Data: " + data);
         System.out.println("Key: " + key);
-        System.out.println("Error Index: " + errorIndex);
         System.out.println("Coding Type: " + codingType);
-
-        crcAlgorithmCall();
     }
 
     private void crcAlgorithmCall(){
-         encodedData = CRCModel.encodeData(data, key);
-         validEncode = CRCModel.checkData(encodedData, key);
 
-         setData();
+        encodedData = "NaN";
+        if(!BinaryCheck.validBinary(data)){
+            encodedData = "Not a Valid Input";
+            return;
+        }
+
+         if (codingType.equalsIgnoreCase("encoding")){
+             encodedData = CRCModel.encodeData(data, key);
+
+         }else {
+             encodedData = "Error 404.\n Decoding Coming Soon...";
+             validEncode = false;
+             return;
+         }
+
+         if (senderErrorCB.isSelected()){
+             char[] tampered = data.toCharArray();
+             tampered[2] = (tampered[2] == '1') ? '0' : '1';
+             String received = new String(tampered);
+
+             encodedData = received;
+         }
+
+         validEncode = CRCModel.checkData(encodedData, key);
     }
 
     private void setData(){
-        receiverDataTF.setText("Encoded Data: " + encodedData);
+        errorPicPane.getStyleClass().removeAll();
+        errorPicPane.getStyleClass().remove("typing-image");
+
+        receiverDataTF.setText(encodedData);
         errorMessage.setText(validEncode ? "No error detected. 😘" : "Error detected! 😑");
+
+        if (validEncode){
+            errorPicPane.getStyleClass().remove("slap-image");
+            errorPicPane.getStyleClass().add("kiss-image");
+        }else {
+            errorPicPane.getStyleClass().remove("kiss-image");
+            errorPicPane.getStyleClass().add("slap-image");
+        }
     }
 
     public void sendButton(ActionEvent actionEvent) {
         collectData();
+        printData();
+        crcAlgorithmCall();
+        setData();
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initToggle();
+        errorPicPane.getStyleClass().add("typing-image");
     }
 }
